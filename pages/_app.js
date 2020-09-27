d9f11993e54ecbfb39443509553cfae94c7a913a
Alex Strive <alexstrive@pm.me>
2020-09-27 20:20:08 +0300

    Batch commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'shards-ui/dist/css/shards.min.css'
+
+import Navbar from '../components/Navbar'
+import { Provider as AuthProvider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthProvider session={pageProps.session}>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
+  )
 }
 
 export default MyApp
