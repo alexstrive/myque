a6fdbed231249d5f5f85f475353c76d5efff995e
Alex Strive <alexstrive@pm.me>
2020-09-28 22:54:01 +0300

    remove auth0 authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,11 +9,12 @@ connect()
 
 const options = {
   providers: [
-    Providers.Auth0({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      domain: process.env.AUTH0_DOMAIN,
-    }),
+    // Rest in peace
+    // Providers.Auth0({
+    //   clientId: process.env.AUTH0_CLIENT_ID,
+    //   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    //   domain: process.env.AUTH0_DOMAIN,
+    // }),
     Providers.Credentials({
       name: 'Credentials',
       credentials: {
